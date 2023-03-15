6302c923878dcff9eab92f672b9c953c6d9d3d8b
Daiana Mendes <103112318+daianamendesss@users.noreply.github.com>
2023-03-15 10:42:04 -0300

    Add files via upload

---
+++ LP2-Projeto Final/Psf/src/psf/Consultas.java
@@ -0,0 +1,57 @@
+
+package psf;
+
+/**
+ *
+ * @author 15385404608
+ */
+public class Consultas {
+   private Paciente objPaciente; 
+   private Medicos objMedico;
+   private String dataEntrada;
+
+    /**
+     * @return the objPaciente
+     */
+    public Paciente getObjPaciente() {
+        return objPaciente;
+    }
+
+    /**
+     * @param objPaciente the objPaciente to set
+     */
+    public void setObjPaciente(Paciente objPaciente) {
+        this.objPaciente = objPaciente;
+    }
+
+    /**
+     * @return the objMedico
+     */
+    public Medicos getObjMedico() {
+        return objMedico;
+    }
+
+    /**
+     * @param objMedico the objMedico to set
+     */
+    public void setObjMedico(Medicos objMedico) {
+        this.objMedico = objMedico;
+    }
+
+    /**
+     * @return the dataEntrada
+     */
+    public String getDataEntrada() {
+        return dataEntrada;
+    }
+
+    /**
+     * @param dataEntrada the dataEntrada to set
+     */
+    public void setDataEntrada(String dataEntrada) {
+        this.dataEntrada = dataEntrada;
+    }
+   
+    
+}
+
---
+++ LP2-Projeto Final/Psf/src/psf/Medicos.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package psf;
+
+/**
+ *
+ * @author 15385404608
+ */
+public class Medicos {
+    private String nome;
+    private String crm;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     
+
+    /**
+     * @return the crm
+     */
+    public String getCrm() {
+        return crm;
+    }
+
+    /**
+     * @param crm the crm to set
+     */
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+}
+
